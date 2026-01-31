2ec9949a7d71538f24a3f2bd7489acb07308059c
lixkote <95425619+lixkote@users.noreply.github.com>
2026-01-31 15:49:39 +0100

    Remove unused shit

--- MdeModulePkg/Application/UiApp/DeviceNameFromProduct.c
+++ MdeModulePkg/Application/UiApp/DeviceNameFromProduct.c
@@ -2,39 +2,8 @@
 #include <Library/BaseLib.h>
 
 /**
-  Case-insensitive string comparison function.
-
-  @param  Str1   First null-terminated string to compare.
-  @param  Str2   Second null-terminated string to compare.
-
-  @retval 0   The strings are equal (case-insensitive).
-  @retval !=0 The strings are not equal.
+  Returns a hardcoded device name.
 **/
-STATIC
-INTN
-StriCmp (
-  IN CONST CHAR16  *Str1,
-  IN CONST CHAR16  *Str2
-  )
-{
-  CHAR16  Char1;
-  CHAR16  Char2;
-
-  while (*Str1 != L'\0') {
-    Char1 = CharToUpper (*Str1);
-    Char2 = CharToUpper (*Str2);
-
-    if (Char1 != Char2) {
-      return Char1 - Char2;
-    }
-
-    Str1++;
-    Str2++;
-  }
-
-  return CharToUpper (*Str1) - CharToUpper (*Str2);
-}
-
 VOID
 GetDeviceNameFromProduct (
   IN      CHAR16                  *Product,
@@ -44,3 +13,4 @@ GetDeviceNameFromProduct (
 {
     StrCatS (*DeviceName, BufferSize / sizeof (CHAR16), L"Acer Spin CP713-1WN");
 }
+
